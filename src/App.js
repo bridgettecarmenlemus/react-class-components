60218850a7072ef1b1b1ecf038d6c1a45a195c2f
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-08-15 23:03:53 -0400

    updated the read me and fixed syntax errors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import FunctionalComponent from './FunctionalComponent';
-import ClassComponent from './ClassComponent';
+import React, { Component } from "react";
+import "./App.css";
+import FunctionalComponent from "./FunctionalComponent";
+import ClassComponent from "./ClassComponent";
 
-
-
-
-let count = 1
+let count = 1;
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        components go here
-       <FunctionalComponent count={count}/>
-       <ClassComponent count={count}/>
-
+          components go here
+          <FunctionalComponent passCount={count} />
+          <ClassComponent count={count} />
         </header>
       </div>
     );
--- src/FunctionalComponent.js
+++ src/FunctionalComponent.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-export default function FunctionalComponent({count}) {
-  return ( <>Omg! this is so functional! {count}</>)
-  ;
+export default function FunctionalComponent({ passCount }) {
+  const [count, setCount] = useState(passCount);
+  const [coffee, setCoffee] = useState([]);
+
+  useEffect(() => {
+    let url = "https://api.sampleapis.com/coffee/hot";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        // do something with data
+        setCoffee(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Omg! this is so functional! {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        add one
+      </button>
+      {/* mapp through coffee */}
+      {coffee.map((coffee) => (
+        <h1 key={coffee.id}>{coffee.title}</h1>
+      ))}
+    </>
+  );
 }
